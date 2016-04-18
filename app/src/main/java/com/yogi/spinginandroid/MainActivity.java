2e349e1c5ebea789d84e4bcfb1edc4dfd0339281
Yogesh Chaudhary <yyogesh.chaudhary@gmail.com>
2016-04-18 15:55:11 +0530

    Demo Code Added

--- app/src/main/java/com/yogi/spinginandroid/MainActivity.java
+++ app/src/main/java/com/yogi/spinginandroid/MainActivity.java
@@ -1,7 +1,15 @@
 package com.yogi.spinginandroid;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.yogi.spinginandroid.dao.Greeting;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new HttpRequestTask().execute();
     }
+
+    private class HttpRequestTask extends AsyncTask<Void, Void, Greeting> {
+        @Override
+        protected Greeting doInBackground(Void... params) {
+            try {
+                final String url = "http://rest-service.guides.spring.io/greeting";
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Greeting greeting = restTemplate.getForObject(url, Greeting.class);
+                return greeting;
+            } catch (Exception e) {
+                Log.e("MainActivity", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Greeting greeting) {
+            TextView greetingIdText = (TextView) findViewById(R.id.id_value);
+            TextView greetingContentText = (TextView) findViewById(R.id.content_value);
+            greetingIdText.setText(greeting.getId());
+            greetingContentText.setText(greeting.getContent());
+        }
+
+    }
+
 }
---
+++ app/src/main/java/com/yogi/spinginandroid/dao/Greeting.java
@@ -0,0 +1,19 @@
+package com.yogi.spinginandroid.dao;
+
+/**
+ * Created by ril on 4/18/16.
+ */
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
